2549f55dd94bb0714d4d6ec6d393571f0f611d9f
Redd2019 <anton-mikerin@yandex.ru>
2023-01-23 16:04:35 +0300

    Выполнено задания 8 и 9

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
+        task9();
     }
 
     public static void task1() {
         System.out.println("Задание 1");
-        for(int n = 1; n <= 10; n = n + 1){
+        for (int n = 1; n <= 10; n = n + 1) {
             System.out.println("Итерация цикла " + n);
         }
     }
@@ -25,7 +27,7 @@ public class Main {
 
     public static void task3() {
         System.out.println("Задание 3");
-        for (int a = 0; a <= 17; a = a+2) {
+        for (int a = 0; a <= 17; a = a + 2) {
             System.out.println(a);
         }
     }
@@ -37,24 +39,45 @@ public class Main {
         }
     }
 
-    public static void task5(){
+    public static void task5() {
         System.out.println("Задание 5");
-        for (int a = 1904; a <= 2096; a = a + 4){
+        for (int a = 1904; a <= 2096; a = a + 4) {
             System.out.println(a + " год является високосным.");
         }
     }
 
     public static void task6() {
         System.out.println("Задание 6");
-        for (int a = 7; a <= 98; a = a + 7){
+        for (int a = 7; a <= 98; a = a + 7) {
             System.out.println(a);
         }
     }
 
     public static void task7() {
         System.out.println("Задание 7");
-        for (int a = 1; a <= 512; a = a * 2){
+        for (int a = 1; a <= 512; a = a * 2) {
             System.out.println(a);
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задание 8");
+        int money = 29000;
+        int total = 0;
+        for (int a = 0; a <= 12; a++) {
+            total = total + money;
+            System.out.println("Месяц " + a + ", сумма накоплений равна " + total + " рублей.");
+        }
+    }
+
+    public static void task9() {
+        System.out.println("Задание 9");
+        int money = 29000;
+        int total = 0;
+        for (int a = 0; a <= 12; a++) {
+            total = total + total / 100;
+            total = total + money;
+            System.out.println("Месяц " + a + ", сумма накоплений равна " + total + " рублей.");
         }
     }
 }
