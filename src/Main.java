e612ebef0c2b2788f12315d3dbfdd667a873ac83
Redd2019 <anton-mikerin@yandex.ru>
2023-01-23 14:37:55 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        for(int n = 1; n <= 10; n = n + 1){
+            System.out.println("Итерация цикла " + n);
+        }
     }
 }
