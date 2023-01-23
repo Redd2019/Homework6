562768b2082422bab0c7108cd1e074a6362fbdf5
Redd2019 <anton-mikerin@yandex.ru>
2023-01-23 15:21:04 +0300

    Выполнено задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -15,6 +16,13 @@ public class Main {
         System.out.println("Задание 2");
         for (int a = 10; a >= 1; a = a - 1) {
             System.out.println("Итерация цикла " + a);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+        for (int a = 0; a <= 17; a = a+2) {
+            System.out.println(a);
         }
     }
 }
